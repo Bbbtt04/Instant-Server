acbb263c885fc5f5254fc3ab9b9b7a3c35b4a537
Bbbtt04 <2393714045@qq.com>
2023-03-10 16:30:14 +0800

    基础配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      username: 'root',
+      password: '123456',
+      database: 'im',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './common/transfom.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // 全局拦截器
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  // swagger
+  const options = new DocumentBuilder()
+    .setTitle('Instant Messaging')
+    .setDescription('冰糖的即时通讯系统')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000).then(() => {
+    console.log(
+      '后台服务已经开启：' +
+        'http://localhost:3000\n' +
+        'swagger文档：' +
+        'http://localhost:3000/api',
+    );
+  });
 }
 bootstrap();
